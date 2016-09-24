2c3c848c47e849c6c0099f72f251c1a05a23645d
Naveeen Kumar Sangi <nkprince007@gmail.com>
2016-09-25 00:39:53 +0530

    Cache service added

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -61,7 +61,7 @@ class GenusController extends Controller {
             throw $this->createNotFoundException('genus not found');
         }
 
-        $markdownParser = new MarkdownTransformer($this->get('markdown.parser'));
+        $markdownParser = $this->get('app.markdown_transformer');
         $funFact = $markdownParser->parse($genus->getFunFact());
 
         $recentNotes = $em->getRepository('AppBundle:GenusNote')->findAllRecentNotesForGenius($genus);
--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -9,13 +9,26 @@
 namespace AppBundle\Service;
 
 
+use Doctrine\Common\Cache\Cache;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+
 class MarkdownTransformer
 {
-    public function __construct($markdownParser) {
+    public function __construct(MarkdownParserInterface $markdownParser, Cache $cache) {
+        $this->cache = $cache;
         $this->markdownParser = $markdownParser;
     }
 
     public function parse($str) {
-        return $this->markdownParser->transformMarkdown($str);
+        $cache = $this->cache;
+        $key = md5($str);
+        if ($cache->contains($key)) {
+            return $cache->fetch($key);
+        }
+        
+        $str = $this->markdownParser
+            ->transformMarkdown($str);
+        $cache->save($key, $str);
+        return $str;
     }
 }
