675c998e07271300e09ea398b7aafd1aaeab7291
Naveeen Kumar Sangi <nkprince007@gmail.com>
2016-09-25 03:38:49 +0530

    Autowiring the services based on type-hints and linking the extension

--- src/AppBundle/Twig/MarkdownExtension.php
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -8,7 +8,19 @@
 
 namespace AppBundle\Twig;
 
+use AppBundle\Service\MarkdownTransformer;
+
 class MarkdownExtension extends \Twig_Extension {
+
+    private $markdownTransformer;
+
+    public function __construct(MarkdownTransformer $markdownTransformer) {
+        $this->markdownTransformer = $markdownTransformer;
+    }
+
+    public function parseMarkdown($str) {
+        return $this->markdownTransformer->parse($str);
+    }
 
     public function getName() {
         return 'app_markdown';
@@ -16,11 +28,8 @@ class MarkdownExtension extends \Twig_Extension {
 
     public function getFilters() {
         return [
-            new \Twig_SimpleFilter('markdownify', array($this, 'parseMarkdown'))
+            new \Twig_SimpleFilter('markdownify', array($this, 'parseMarkdown'), ['is_safe' => ['html']])
         ];
     }
 
-    public function parseMarkdown($str) {
-        return strtoupper($str);
-    }
 }
