7917133d98f669cb051155c8284137902595db86
Naveeen Kumar Sangi <nkprince007@gmail.com>
2016-09-26 16:46:38 +0530

    Implementing UserInterface methods

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nkprince007
+ * Date: 26/09/16
+ * Time: 4:46 PM
+ */
+
+namespace AppBundle\Controller;
+
+
+class SecurityController
+{
+
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nkprince007
+ * Date: 26/09/16
+ * Time: 4:33 PM
+ */
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface {
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+
+}
